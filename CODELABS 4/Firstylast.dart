c709b7e4213c60480075060889ab734bd0f0ed0e
Maritza Ramirez <maritzaramires20@gmail.com>
2024-06-01 10:30:29 -0600

    primero y ultimo

---
+++ CODELABS 4/Firstylast.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Crear un Iterable de cadenas
+  Iterable<String> iterable = const ['Salad', 'Popcorn', 'Toast'];
+
+  // Imprimir el primer elemento del Iterable
+  print('El primer elemento es ${iterable.first}');
+
+  // Imprimir el último elemento del Iterable
+  print('El último elemento es ${iterable.last}');
+}
